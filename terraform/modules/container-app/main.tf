52924a5c40daaeffab98ee7fc5dadea8244eefad
joseluispg1410 <96244693+joseluispg1410@users.noreply.github.com>
2025-12-19 00:56:02 +0100

    Refactor container app module configuration

--- terraform/modules/container-app/main.tf
+++ terraform/modules/container-app/main.tf
@@ -1,16 +1,13 @@
-# Container App module configuration
-# Creates a container app environment and deploys a sample app
-
 # Container App Environment
 resource "azurerm_container_app_environment" "env" {
-  name                = "cae-${var.env_name}" # Unique name for the environment
+  name                = "cae-${var.env_name}"
   location            = var.location
   resource_group_name = var.resource_group_name
 }
 
 # Sample Container App
 resource "azurerm_container_app" "app" {
-  name                         = "app-${var.env_name}" # Unique app name
+  name                         = "app-${var.env_name}"
   resource_group_name          = var.resource_group_name
   container_app_environment_id = azurerm_container_app_environment.env.id
   revision_mode                = "Single"
@@ -20,17 +17,19 @@ resource "azurerm_container_app" "app" {
       name   = "sample-app"
       image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
 
-      resources {
-        cpu    = 0.25
-        memory = "0.5Gi"
-      }
+      cpu    = 0.25
+      memory = "0.5Gi"
     }
   }
 
-  # Configure ingress to make the app publicly accessible
   ingress {
     external_enabled = true
     target_port      = 80
     transport        = "auto"
+
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
   }
 }
