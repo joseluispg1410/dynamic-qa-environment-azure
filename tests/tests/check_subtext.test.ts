9610f108d555baff58f7029bb853d2a02f2ae6d3
José Luis Pérez García <>
2025-12-20 00:04:39 +0100

    Add console log for debug url

--- tests/tests/check_subtext.test.ts
+++ tests/tests/check_subtext.test.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Homepage contain subtext', async ({ page }) => {
+  console.log('URL actual:', page.url());
   const elemento = page.locator('#subText');
   await expect(elemento).toContainText('To Learn more, follow our docs');
 });
--- tests/tests/ui.spec.ts
+++ tests/tests/ui.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 // Simple smoke test for homepage
 test('Homepage loads', async ({ page }) => {
+  console.log('URL actual:', page.url());
   const elemento = page.locator('#titleText');
   await expect(elemento).toHaveText('Your Azure Container Apps app is live');
 });
